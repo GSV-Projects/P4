02fcaa6c18e22403f1b24bbe693636f73b9bd9dd
FrederikSperling <frederik.sperling32121@gmail.com>
2026-04-01 15:12:23 +0200

    Fremskidt på at bygge ast Nuværrende problem parsetræ giver en flad struktur, der ikke repræsenterer venstre eller højre associativitet. I Transformeren skal vi så lave den træ struktur gl us o7
    Co-authored-by: Mads <MadsMoneyCrank@users.noreply.github.com>
    Co-authored-by: Venil0u <Venil0u@users.noreply.github.com>

--- tadpole/parser_lexer_lark.py
+++ tadpole/parser_lexer_lark.py
@@ -3,25 +3,25 @@ start: program
 
 program: (stmt | def)*
 
-stmt: IDENT stmt_suffix ";"
+stmt: IDENT "." call ";"    -> method_call 
+    | IDENT "=" rvalue ";"  -> assign
+    | IDENT "(" (expr ("," expr)*)? ")" ";"     -> call
     | "while" "(" expr ")" "do" "{" stmt* "}"
-    | "if" "(" expr ")" "then" "{" stmt* "}" ("else" "{" stmt* "}")?
-    | "stop" ";"
-    | "skip" ";"
+    | "if" "(" expr ")" "then" "{" stmt* "}" ("else" "{" stmt* "}")?    -> ifs #
+    | "stop" ";"    -> stop #
+    | "skip" ";"    -> skip #
     | "return" expr ";"
 
-stmt_suffix: "." call
-           | "=" rvalue -> assign
-           | "(" (expr ("," expr)*)? ")"
 
 rvalue: "[" (expr ("," expr)*)? "]"
-      | expr | "{" IDENT ("," IDENT)* ":" term ("," term)* "}"
+    | expr
+    | "{" IDENT ("," IDENT)* ":" term ("," term)* "}"
 
 
 type: "bool"
-    | "float" -> float
-    | "int" -> int
-    | "string" -> hejsa
+    | "float"
+    | "int"
+    | "string"
     | "table"
     | "column"
     | "[" type "]"
@@ -35,31 +35,32 @@ def: "function" IDENT "(" param ")" ret
 ret: "returns" type "{" stmt* "}"
    | "{" stmt* "}"
 
-expr: and_expr ("or" and_expr)*
+expr: and_expr ("or" and_expr)*     -> expr
 
-and_expr: not_expr ("and" not_expr)*
+and_expr: not_expr ("and" not_expr)*    -> and_expr
 
-not_expr: ("not")* eq_expr
+not_expr: ("not")* eq_expr  -> not_expr
 
-eq_expr: plus_expr (("==" | "/=" | "<" | "<=" | ">" | ">=") plus_expr)?
+eq_expr: additive_expr (("==" | "/=" | "<" | "<=" | ">" | ">=") additive_expr)? -> eq_expr
 
-plus_expr: mult_expr (("+" | "-") mult_expr)* -> add
+additive_expr: multiplicative_expr (("+" | "-") multiplicative_expr)* -> additive_expr
 
-mult_expr: exp_expr (("*" | "/" | "mod") exp_expr)*
+multiplicative_expr: expo_expr (("*" | "/" | "mod") expo_expr)* -> multiplicative_expr
 
-exp_expr: unary_expr ("^" unary_expr)*
+expo_expr: unary_expr ("^" unary_expr)* -> expo_expr
 
-unary_expr: ("-")* term
+unary_expr: ("-")* term -> unary_expr
 
 term: IDENT ("(" (expr ("," expr)*)? ")" | "[" expr "]")?
-     | "." call
-     | FLOAT
-     | NUM
-     | STRING
-     | BOOL
-     | NA
-     | "(" expr ")"
+    | "." call
+    | FLOAT
+    | NUM
+    | STRING
+    | BOOL
+    | NA
+    | "(" expr ")"
 
+# Token specification 
 // --- Keywords ---
 WHILE: "while"
 DO: "do"
@@ -71,7 +72,6 @@ SKIP: "skip"
 RETURN: "return"
 FUNCTION: "function"
 RETURNS: "returns"
-
 AND: "and"
 OR: "or"
 NOT: "not"
@@ -85,7 +85,8 @@ GREATEQ: ">="
 LESS: "<"
 GREAT: ">"
 ADD: "+"
-SUB: "-"
+SUB: "-"    # subtraction
+#NEG: "-"    # unary negative
 MULT: "*"
 DIVIDE: "/"
 ASSIGN: "="
@@ -110,14 +111,19 @@ STRING: /"([^"\\]|\\.)*"/
 """
 
 code = """
-var1 = 1+1;
+a = 5 + 5 + 5;
 """
 
 from lark import Lark
 from parsertransformer import MyTrans
 
+def transformtree(tree):
+    return MyTrans().transform(tree)
+
 parser = Lark(grammar, parser="lalr", strict=True)
 
 
-tree = parser.parse(code)
-print(tree.pretty())
+parsetree = parser.parse(code)
+#result = transformtree(parsetree)
+print("Parse \n", parsetree.pretty())
+#print("AST \n", result.pretty())
--- tadpole/parsertransformer.py
+++ tadpole/parsertransformer.py
@@ -1,5 +1,94 @@
 from lark import Lark, Transformer, v_args, Tree
 
+
 class MyTrans(Transformer):
-    def start(self, children):
-        return Tree("start", children)
+    # Terminals for types
+    def NUMBER(self, children):
+        return int(children.value)
+
+    def IDENT(self, children):
+        return str(children.value)
+    
+    def FLOAT(self, children):
+        return float(children.value)
+
+    def BOOL(self, children):
+        return bool(children.value) 
+    
+    def STRING(self, children):
+        return str(children.value)
+    
+    def NA(self, children):
+        return str(children.value)
+    
+    # Starting node needed to create the tree
+    def start(self, c):
+        return c[0]
+    
+    def program(self, children):
+        return children[0]
+    
+    def method_call(self, c):
+        return Tree(".", c)    
+
+    def assign(self, c):
+        return Tree("=", c)
+    
+    def call(self, c):
+        return Tree("call", c)
+    
+    def rvalue(self, c):
+        return c[0]
+    
+    def expr(self, c):
+        if (len(c) == 1):
+            return c[0]
+        return Tree("or", c)
+    
+    def and_expr(self, c):
+        if (len(c)==1):
+            return c[0]
+        return Tree("and", c)
+    
+    def not_expr(self, c):
+        if (len(c)==1):
+            return c[0]
+        return Tree("not", c)
+
+    # Equal expressions
+    def eq_expr(self, c):
+        return c[0]
+    
+    # Additive expressions
+    def additive_expr(self, c):
+        return self.fold_left(c)
+    
+    # Multiplicative expressions
+    def multiplicative_expr(self, c):
+        return self.fold_left(c)
+
+    # Exponential expression
+    def expo_expr(self, c):
+        if (len(c)==1):
+            return c[0]
+        return Tree("^", c)
+    
+    def unary_expr(self, c):
+        return c[0]
+    
+    def term(self, c):
+        return c[0]
+
+    def fold_left(self, items):
+        if len(items) == 1:
+            return items[0]
+
+        left = items[0]
+        i = 1
+        while i < len(items):
+            op = items[i]
+            right = items[i+1]
+            left = Tree(op.value, [left, right])
+            i += 2
+
+        return left
