7e8b5dd6029a60efc380fa3f9e53f85fe6c4d59a
David Bach <60426203+DrMachoo@users.noreply.github.com>
2026-05-05 11:37:28 +0200

    test og build ting

--- Test/test_evaluator.py
+++ Test/test_evaluator.py
@@ -2,3 +2,6 @@ from lark import Tree, Token
 from tadpole.interpreter import Interpreter
 
 
+# Test if additive expressions return correct result (Maybe do a few)
+
+#
--- Test/test_typechecker.py
+++ Test/test_typechecker.py
@@ -3,8 +3,8 @@ from tadpole.type_checker import Typechecker
 
 
 assign_tree = Tree(Token('RULE', 'program'), [Tree('assign', [Token('IDENT', 'a'), Token('INT', '5')])])
-Typechecker.check_p(assign_tree)
+typechecker = Typechecker()
+typechecker.check_p(assign_tree)
 
 def test_vtable():
-    
-    assert Typechecker.vtable['a'] == int
+    assert typechecker.vtable['a'] == int
