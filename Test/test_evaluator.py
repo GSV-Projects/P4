c994caf527e0a7c10e14d600f06a3e7a26dacc9d
David Bach <60426203+DrMachoo@users.noreply.github.com>
2026-05-05 11:43:39 +0200

    todo

--- Test/test_evaluator.py
+++ Test/test_evaluator.py
@@ -2,6 +2,13 @@ from lark import Tree, Token
 from tadpole.interpreter import Interpreter
 
 
-# Test if additive expressions return correct result (Maybe do a few)
-
-#
+''' TODO:
+        - Create test for aritmetiske operationer
+            +,-,*,/
+            Edge cases man kan checke om de opfører sig korrekt
+            Kun integer operationer
+            Mixed float/int
+            Division med 0
+            Exponenter med store tal
+            mod med 0
+'''
