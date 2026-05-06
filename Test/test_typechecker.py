050d79294bf703aaaeee6483f6e6d61cda3e7ff7
David Bach <60426203+DrMachoo@users.noreply.github.com>
2026-05-06 11:45:42 +0200

    yes

--- Test/test_typechecker.py
+++ Test/test_typechecker.py
@@ -126,28 +126,184 @@ def test_add_int_NA():
 
 def test_array_indexing_with_float():
     # Index with a float
-    pass
+
+    # Setup
+    tree = Tree(Token('RULE', 'program'), [
+        Tree('assign', [Token('IDENT', 'a'), Tree('array', [Token('INT', '1'), Token('INT', '2'), Token('INT', '3')])]), Tree('assign_index', [Token('IDENT', 'a'), Token('FLOAT', '2.2'), Token('INT', '5')])])
+    typechecker = Typechecker()
+
+    with pytest.raises(Exception) as excinfo:
+        typechecker.check_p(tree)
+
+    # What the syntax_tree represents
+    '''
+    a = [1,2,3];
+    a[2.2] = 5;
+    '''
+
+    # Expected output
+    assert "Did not parse an integer for array indexing" in str(excinfo.value)
 
 def test_array_indexing_value():
-    # Value that is returned
-    pass
+    # Testing value that is returned and type of array
+
+    # Setup
+    tree = Tree(Token('RULE', 'program'), [
+        Tree('assign', [Token('IDENT', 'a'), Tree('array', [Token('INT', '1'), Token('INT', '2'), Token('INT', '3')])]), Tree('assign', [Token('IDENT', 'b'), Tree('index', [Token('IDENT', 'a'), Token('INT', '2')])])])
+    typechecker = Typechecker()
+    typechecker.check_p(tree)
+
+    # What the syntax_tree represents
+    '''
+    a = [1,2,3];
+    b = a[2];
+    '''
+
+    # Expected output
+    assert (typechecker.vtable['b'] == int and typechecker.vtable['a'] == [int])
 
 def test_array_creation_exception():
-    # Not all types are the same
-    pass
+    # Testing array wher not all types are the same
+
+    # Setup
+    tree = Tree(Token('RULE', 'program'), [
+        Tree('assign', [Token('IDENT', 'a'), Tree('array', [Token('INT', '1'), Token('INT', '2'), Token('FLOAT', '3.4')])])])
+    typechecker = Typechecker()
+
+    with pytest.raises(Exception) as excinfo:
+        typechecker.check_p(tree)
+
+    # What the syntax_tree represents
+    '''
+    a = [1,2,3.4];
+    '''
+
+    # Expected output
+    assert "Not all elements of array are of the same type" in str(excinfo.value)
 
 def test_array_creation_correct_type():
-    # Correct array creation
+    # Testing correct array creation
+
+    # Setup
+    tree = Tree(Token('RULE', 'program'), [
+        Tree('assign', [Token('IDENT', 'a'), Tree('array', [Token('INT', '1'), Token('INT', '2'), Token('INT', '3')])])])
+    typechecker = Typechecker()
+
+    typechecker.check_p(tree)
+
+    # What the syntax_tree represents
+    '''
+    a = [1,2,3];
+    '''
+
+    # Expected output   
+    assert typechecker.vtable['a'] == [int]
+
+def test_func_assign_locally_globally():
+    # Testing assignment of varible local and global
+
+    # Setup
+    tree = Tree(Token('RULE', 'program'), [Tree('assign', [Token('IDENT', 'a'), Token('INT', '5')]), Tree('func_def_ret', [Token('IDENT', 'myfunc'), Tree('param', [Tree('param_item', [Token('TYPE_FLOAT', 'float'), Token('IDENT', 'a')])]), Token('TYPE_FLOAT', 'float'), Tree('return', [Token('IDENT', 'a')])]), Tree('assign', [Token('IDENT', 'b'), Tree('call', [Token('IDENT', 'myfunc'), Token('FLOAT', '2.5')])])])
+    typechecker = Typechecker()
+
+    typechecker.check_p(tree)
+
+    # What the syntax_tree represents
+    '''
+    a = 5;
+
+    function myfunc(float a) returns float{
+    return a;
+    }
+
+    b = myfunc(2.5);
+    '''
+
+    # Expected output
+    assert (typechecker.vtable['a'] == int and typechecker.vtable['b'] == float)
+
+def test_func_return_out_of_scope():
+    # Testing return out of a function 
+
+    # Setup
+    tree = Tree(Token('RULE', 'program'), [Tree('return', [Token('INT', '3')])])
+    typechecker = Typechecker()
+
+    with pytest.raises(Exception) as excinfo:
+        typechecker.check_p(tree)
+
+    # What the syntax_tree represents
+    '''
+    return 3;
+    '''
+
+    # Expected output
+    assert "doesnt match with function return type" in str(excinfo.value)
+
+def test_func_return_wrong_type():
+    # Testing return with wrong type 
+
+    # Setup
+    tree = Tree(Token('RULE', 'program'), [Tree('func_def_ret', [Token('IDENT', 'myfunc'), Tree('param', [Tree('param_item', [Token('TYPE_FLOAT', 'float'), Token('IDENT', 'a')])]), Token('TYPE_FLOAT', 'float'), Tree('body', [Tree('assign', [Token('IDENT', 'a'), Token('INT', '5')]), Tree('return', [Token('IDENT', 'a')])])]), Tree('assign', [Token('IDENT', 'b'), Tree('call', [Token('IDENT', 'myfunc'), Token('FLOAT', '2.5')])])])
+    typechecker = Typechecker()
+
+    with pytest.raises(Exception) as excinfo:
+        typechecker.check_p(tree)
+
+    # What the syntax_tree represents
+    '''
+    function myfunc(float a) returns float{
+        a = 5;
+        return a;
+    }
+
+    b = myfunc(2.5);
+    '''
+
+    # Expected output
+    assert "doesnt match with function return type" in str(excinfo.value)
+
+def test_if_not_bool():
+    # Testing if its possible to use a non bool in the if stmt
+
+    # Setup
+    tree = Tree(Token('RULE', 'program'), [Tree('assign', [Token('IDENT', 'a'), Token('INT', '3')]), Tree('if', [Token('IDENT', 'a'), Tree('then', [Tree('assign', [Token('IDENT', 'b'), Token('INT', '5')])])])])
+    typechecker = Typechecker()
+
+    with pytest.raises(Exception) as excinfo:
+        typechecker.check_p(tree)
+
+    # What the syntax_tree represents
+    '''
+    a = 3;
+
+    if (a) then {
+        b = 5;
+    }
+    '''
+
+    # Expected output
+    assert "is not a boolean expression" in str(excinfo.value)
+
+def test_func_multiple_params():
+    # Testing if we get an error is amont of actual and formal doesnt match
     pass
 
-def func_assign_locally_globally():
-    # assign varible local and global and assert the type both times
+def test_func_scope_isolation():
+    # function f() { x = 10; } x = 5; f(); x should still be 5
     pass
 
-def func_return_values():
-    # Test return values are correct
+def test_NA_array():
     pass
 
-def if_not_bool():
-    # if (5) for example
+def test_check_logical():
+    pass
+
+def test_while_not_bool():
+    pass
+
+def test_table_type():
+    pass
+
+def test_column_types():
     pass
