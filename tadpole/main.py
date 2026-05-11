d68aadc359d21bc38c8cfca4a2ea9e58b9625fbc
FrederikSperling <frederik.sperling32121@gmail.com>
2026-05-11 15:37:11 +0200

    included a small example for main.py

--- tadpole/main.py
+++ tadpole/main.py
@@ -7,14 +7,11 @@ from tadpole.type_checker import Typechecker
 
 # The code to be executed:
 code = """
-mytab = {
-col1 : [1,3,5,7];
-col2 : [2.2, 4.4, 6.6, 8.8];
-col3 : ["one", "two", "three", "four"];
-col4 : [true, false, false, true];
-};
 
-test = mytab.frequency("col1", 1);
+if(true) then {
+    a = 3;
+}
+else {a = 5;}
 
 """
 
