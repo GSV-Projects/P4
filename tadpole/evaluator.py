6862036876173b58245eda1e140279798123b996
Venil0u <111545934+Venil0u@users.noreply.github.com>
2026-05-12 09:00:20 +0200

    Add filtercol method to evaluator

--- tadpole/evaluator.py
+++ tadpole/evaluator.py
@@ -24,6 +24,7 @@ class Evaluator():
             "remove":       Table.remove,
             "mutate":       Table.mutate,
             "filter" :      Table.filter,
+            "filtercol":    Table.filtercol,
             "first":        Table.first,
             "last":         Table.last,
             "sort" :        Table.sort,
